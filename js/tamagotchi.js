fc177c01bd30e6f061a2a052b14f162fb257e353
Dan <dnshaw12@gmail.com>
2019-07-02 18:29:37 -0500

    got more images

--- js/tamagotchi.js
+++ js/tamagotchi.js
@@ -5,6 +5,9 @@ class Tamagotchi {
 		this.name = name;
 		this.hunger = 1;
 		this.sleepiness = 1;
-		this.boredum = 1;
+		this.boredom = 1;
+		this.hungry = false;
+		this.sleepy = false;
+		this.bored = false;
 	}
-}
+};
